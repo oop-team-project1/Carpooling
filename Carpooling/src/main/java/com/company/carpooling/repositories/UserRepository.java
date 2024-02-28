26dd8e159a6438abbf76b37090d8b6286f2351bd
Kaloyan <kaloyan.enchev@abv.bg>
2024-02-28 14:41:01 +0200

    Added UserRepository interface and UserRepositoryImpl

--- Carpooling/src/main/java/com/company/carpooling/repositories/UserRepository.java
+++ Carpooling/src/main/java/com/company/carpooling/repositories/UserRepository.java
@@ -1,4 +1,23 @@
 package com.company.carpooling.repositories;
 
+import com.company.carpooling.models.User;
+
+import java.util.List;
+
 public interface UserRepository {
+    List<User> getAll();
+
+    User getById(int id);
+
+    User getByUsername(String username);
+
+    User getByEmail(String email);
+
+    User getByPhoneNumber(String phoneNumber);
+
+    void create(User user);
+
+    void update(User user);
+
+    void deleteUser(int id);
 }
