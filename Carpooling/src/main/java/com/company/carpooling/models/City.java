c33770558ca5b8978c1e78ae72310547ffa2e8ba
ivonasto <ivona.stoyanova.s@gmail.com>
2024-03-01 11:32:56 +0200

    City entity

---
+++ Carpooling/src/main/java/com/company/carpooling/models/City.java
@@ -0,0 +1,30 @@
+package com.company.carpooling.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name="cities")
+public class City {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "city_id")
+    int id;
+
+    @Column(name = "name")
+    String name;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "country_id")
+    Country country;
+
+
+
+}
