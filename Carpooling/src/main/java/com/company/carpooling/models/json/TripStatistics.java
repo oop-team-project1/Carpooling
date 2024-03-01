4f419b52d1937b9e16e012b6d099107547459b86
ivonasto <ivona.stoyanova.s@gmail.com>
2024-03-01 11:33:58 +0200

    Object for trip duration and distance from BingService

---
+++ Carpooling/src/main/java/com/company/carpooling/models/json/TripStatistics.java
@@ -0,0 +1,14 @@
+package com.company.carpooling.models.json;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TripStatistics {
+
+    float travelDistance;
+
+    float travelDuration;
+
+}
