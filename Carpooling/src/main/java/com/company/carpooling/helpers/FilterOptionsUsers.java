9ce898740861d2f20f031278ce9d7802ffe0d759
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-04 14:55:01 +0200

    Added FilterOptionsUsers

---
+++ Carpooling/src/main/java/com/company/carpooling/helpers/FilterOptionsUsers.java
@@ -0,0 +1,30 @@
+package com.company.carpooling.helpers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.util.Optional;
+
+@Getter
+public class FilterOptionsUsers {
+    private Optional<String> username;
+    private Optional<String> phoneNumber;
+    private Optional<String> email;
+    private Optional<String> sortBy;
+    private Optional<String> sortOrder;
+
+    public FilterOptionsUsers() {
+        this(null, null, null, null, null);
+    }
+
+    public FilterOptionsUsers(String username,
+                              String phoneNumber,
+                              String email,
+                              String sortBy,
+                              String sortOrder) {
+        this.username = Optional.ofNullable(username);
+        this.phoneNumber = Optional.ofNullable(phoneNumber);
+        this.email = Optional.ofNullable(email);
+        this.sortBy = Optional.ofNullable(sortBy);
+        this.sortOrder = Optional.ofNullable(sortOrder);
+    }
+}
