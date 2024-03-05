3c011c29b54b8236b5863988f3ae354c720a9d4e
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-05 17:01:53 +0200

    Added CommentFeedback entity

---
+++ Carpooling/src/main/java/com/company/carpooling/models/CommentFeedback.java
@@ -0,0 +1,27 @@
+package com.company.carpooling.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "feedbacks_comments")
+@Getter
+@Setter
+public class CommentFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "feedback_id")
+    private Feedback feedback;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
