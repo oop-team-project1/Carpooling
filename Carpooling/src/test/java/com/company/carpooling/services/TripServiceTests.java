2387e7c07bfc1560fcc0ba5f2fbcfaf587ab248c
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2024-03-05 14:28:01 +0200

    tests for create() method for Trip implemented

--- Carpooling/src/test/java/com/company/carpooling/services/TripServiceTests.java
+++ Carpooling/src/test/java/com/company/carpooling/services/TripServiceTests.java
@@ -1,7 +1,11 @@
 package com.company.carpooling.services;
 
+import com.company.carpooling.exceptions.AuthorizationException;
 import com.company.carpooling.helpers.FilterOptionsTrip;
+import com.company.carpooling.models.Trip;
+import com.company.carpooling.models.User;
 import com.company.carpooling.repositories.TripRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static com.company.carpooling.Helpers.createMockFilterOptionsForTrip;
+import static com.company.carpooling.Helpers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TripServiceTests {
@@ -28,6 +32,40 @@ public class TripServiceTests {
 
         Mockito.verify(mockRepository, Mockito.times(1)).get(mockFilterOptions);
     }
+
+    @Test
+    public void get_Should_ReturnTrip_When_MatchByIdExist() {
+        Trip mockTrip = createMockTrip();
+
+        Mockito.when(mockRepository.get(Mockito.anyInt())).thenReturn(mockTrip);
+        Trip result = tripService.get(mockTrip.getId());
+
+        Assertions.assertEquals(mockTrip,result);
+    }
+
+    @Test
+    public void create_Should_CallRepository_When_UserIsNotBlocked () {
+            Trip trip = createMockTrip();
+            User user = createMockUser();
+
+            Mockito.doNothing().when(mockRepository).create(trip);
+
+            tripService.create(trip,user);
+
+            Mockito.verify(mockRepository, Mockito.times(1)).create(trip);
+    }
+
+    @Test
+    public void create_Should_Throw_When_UserIsBlocked () {
+        Trip trip = createMockTrip();
+        User user = createMockUser();
+        user.setBlocked(true);
+
+        Assertions.assertThrows(AuthorizationException.class, () -> tripService.create(trip,user));
+
+    }
+
+
 
 
 }
