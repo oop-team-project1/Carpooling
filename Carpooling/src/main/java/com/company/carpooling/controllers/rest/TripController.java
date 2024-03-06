fc88be09616b4f243217fbe7cbfa511961d08040
ivonasto <ivona.stoyanova.s@gmail.com>
2024-03-06 10:30:21 +0200

    application lifecycle methods

--- Carpooling/src/main/java/com/company/carpooling/controllers/rest/TripController.java
+++ Carpooling/src/main/java/com/company/carpooling/controllers/rest/TripController.java
@@ -3,14 +3,15 @@ package com.company.carpooling.controllers.rest;
 import com.company.carpooling.exceptions.AuthenticationException;
 import com.company.carpooling.exceptions.AuthorizationException;
 import com.company.carpooling.exceptions.EntityNotFoundException;
+import com.company.carpooling.exceptions.FullyBookedException;
 import com.company.carpooling.helpers.AuthenticationHelper;
 import com.company.carpooling.helpers.FilterOptionsTrip;
 import com.company.carpooling.helpers.TripMapper;
+import com.company.carpooling.models.Application;
 import com.company.carpooling.models.Trip;
 import com.company.carpooling.models.TripDto;
 import com.company.carpooling.models.User;
 import com.company.carpooling.services.TripService;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
@@ -21,6 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @RestController
@@ -54,94 +56,144 @@ public class TripController {
         try {
             authenticationHelper.tryGetUser(encodedString);
             return tripService.get(filterOptionsTrip);
-        } catch(AuthenticationException e) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
-        } catch(AuthorizationException e) {
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (AuthorizationException e) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
         }
     }
 
-@GetMapping("/{id}")
-public Trip get(@PathVariable int id,
-                @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
-    try {
-        authenticationHelper.tryGetUser(encodedString);
-        return tripService.get(id);
-    } catch (EntityNotFoundException e) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    } catch (AuthenticationException e) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    @GetMapping("/{id}")
+    public Trip get(@PathVariable int id,
+                    @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            authenticationHelper.tryGetUser(encodedString);
+            return tripService.get(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
     }
-}
 
-@PostMapping
-public void create(@RequestBody TripDto tripDto,
-                   @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
-    try {
-        User user = authenticationHelper.tryGetUser(encodedString);
-        Trip trip = tripMapper.fromTripDto(tripDto);
-        tripService.create(trip, user);
-    } catch (AuthenticationException e) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    @PostMapping
+    public void create(@RequestBody TripDto tripDto,
+                       @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            Trip trip = tripMapper.fromTripDto(tripDto);
+            tripService.create(trip, user);
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
     }
-}
 
-@PutMapping("/{id}")
-public Trip update(@PathVariable int id,
-                   @RequestBody TripDto tripDto,
-                   @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
-    try {
-        User user = authenticationHelper.tryGetUser(encodedString);
-        Trip trip = tripMapper.fromTripDto(id, tripDto);
-        tripService.update(trip, user);
-        return trip;
-    } catch (EntityNotFoundException e) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-    } catch (AuthenticationException e) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    } catch (AuthorizationException e) {
-        throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    @PutMapping("/{id}")
+    public Trip update(@PathVariable int id,
+                       @RequestBody TripDto tripDto,
+                       @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            Trip trip = tripMapper.fromTripDto(id, tripDto);
+            tripService.update(trip, user);
+            return trip;
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
     }
-}
 
-@DeleteMapping("/{id}")
-public void delete(@PathVariable int id,
-                   @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
-    try {
-        User user = authenticationHelper.tryGetUser(encodedString);
-        tripService.delete(id, user);
-    } catch (EntityNotFoundException e) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-    } catch (AuthenticationException e) {
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-    } catch (AuthorizationException e) {
-        throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id,
+                       @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            tripService.delete(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
     }
-}
 
-@GetMapping("/{id}/passengers")
-public void getPassengers(@PathVariable int id) {
-}
+    @GetMapping("/{id}/applications")
+    public Set<Application> getApplications(@PathVariable int id,
+                                            @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            return tripService.getApplications(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+    }
 
-@PostMapping("/{id}/passengers")
-public void applyForTrip(@PathVariable int id) {
-}
+    @PostMapping("/{id}/applications")
+    public void applyForTrip(@PathVariable int id, @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            tripService.applyForTrip(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+        }
 
-@DeleteMapping("/{id}/passengers")
-public void cancelParticipation(@PathVariable int id) {
-}
+    }
 
-@PostMapping("/{id}/passengers/{passengerId}")
-public void approvePassenger(@PathVariable int id, @PathVariable int passengerId) {
-}
+    @DeleteMapping("/{id}/applications")
+    public void cancelParticipation(@PathVariable int id, @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            tripService.cancelParticipation(id, user);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+        }
+    }
 
-@DeleteMapping("/{id}/passengers/{passengerId}")
-public void rejectPassenger(@PathVariable int id, @PathVariable int passengerId) {
-}
+    @PostMapping("/{id}/applications/{passengerId}")
+    public void approvePassenger(@PathVariable int id, @PathVariable int passengerId, @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            tripService.approvePassenger(id, user, passengerId);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+        } catch (FullyBookedException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+        }
+    }
 
-@PutMapping("/{id}/status")
-public void changeStatus(@PathVariable int id) {
-}
+    @DeleteMapping("/{id}/applications/{passengerId}")
+    public void rejectPassenger(@PathVariable int id, @PathVariable int passengerId, @RequestHeader(value = HttpHeaders.AUTHORIZATION) String encodedString) {
+        try {
+            User user = authenticationHelper.tryGetUser(encodedString);
+            tripService.rejectPassenger(id, user, passengerId);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (AuthenticationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        } catch (AuthorizationException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+        }
+    }
 
 
 }
