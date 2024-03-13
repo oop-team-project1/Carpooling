4bdeb6f65adcb17607c919797825fe6650191949
ivonasto <ivona.stoyanova.s@gmail.com>
2024-03-13 20:11:21 +0200

    Overload method to accept int as value

--- Carpooling/src/main/java/com/company/carpooling/exceptions/EntityDuplicateException.java
+++ Carpooling/src/main/java/com/company/carpooling/exceptions/EntityDuplicateException.java
@@ -4,4 +4,7 @@ public class EntityDuplicateException extends RuntimeException{
     public EntityDuplicateException(String type, String attribute, String value) {
         super(String.format("%s with %s %s already exists.", type, attribute, value));
     }
+    public EntityDuplicateException(String type, String attribute, int value) {
+        super(String.format("%s with %s %d already exists.", type, attribute, value));
+    }
 }
