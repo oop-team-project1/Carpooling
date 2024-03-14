6b8d2d136367e833516cfcdb704a6b71cfc05dc2
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2024-03-14 12:10:59 +0200

    added validation for feedback rating

--- Carpooling/src/main/java/com/company/carpooling/helpers/FeedbackMapper.java
+++ Carpooling/src/main/java/com/company/carpooling/helpers/FeedbackMapper.java
@@ -7,9 +7,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class FeedbackMapper {
 
+    public static final String INVALID_FEEDBACK_ERR = "Feedback must be a number between 1 and 5.";
+
     public Feedback fromFeedbackDto (FeedbackDto feedbackDto) {
         Feedback feedback = new Feedback();
+        checkIfFeedbackIsInValidRange(feedbackDto);
         feedback.setRating(feedbackDto.getRating());
         return feedback;
+    }
+
+    private void checkIfFeedbackIsInValidRange(FeedbackDto feedbackDto) {
+        if (feedbackDto.getRating() < 1 || feedbackDto.getRating() > 5) {
+            throw new IllegalArgumentException(INVALID_FEEDBACK_ERR);
+        }
     }
 }
