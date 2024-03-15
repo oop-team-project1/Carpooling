b10d2872c3f8a44faa147b55e458b2187e024573
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-15 17:04:54 +0200

    Added RegisterDto

---
+++ Carpooling/src/main/java/com/company/carpooling/models/dtos/RegisterDto.java
@@ -0,0 +1,21 @@
+package com.company.carpooling.models.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDto extends LoginDto{
+    @NotEmpty(message = "Username can't be empty!")
+    private String username;
+    @NotEmpty(message = "Password confirmation can't be empty!")
+    private String passwordConfirm;
+    @NotEmpty(message = "First name can't be empty!")
+    @Size(min = 4, max = 32, message = "First name must be between 4 symbols and 32 symbols.")
+    private String firstName;
+    @NotEmpty(message = "Last name can't be empty!")
+    @Size(min = 4, max = 32, message = "Last name must be between 4 symbols and 32 symbols.")
+    private String lastName;
+}
