b6ff9cec5882eeb19424b390e7b51228b81eaaa6
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-15 17:05:34 +0200

    Added fromDtoRegister method in UserMapper

--- Carpooling/src/main/java/com/company/carpooling/helpers/UserMapper.java
+++ Carpooling/src/main/java/com/company/carpooling/helpers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.company.carpooling.helpers;
 
 import com.company.carpooling.models.User;
+import com.company.carpooling.models.dtos.RegisterDto;
 import com.company.carpooling.models.dtos.UserDto;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +22,16 @@ public class UserMapper {
         user.setEmail(userDto.getEmail());
         user.setPhoneNumber(userDto.getPhoneNumber());
         user.setAdmin(false);
+        return user;
+    }
+
+    public User fromDtoRegister(RegisterDto registerDto) {
+        User user = new User();
+        user.setUsername(registerDto.getUsername());
+        user.setFirstName(registerDto.getFirstName());
+        user.setLastName(registerDto.getLastName());
+        user.setPassword(registerDto.getPassword());
+        user.setEmail(registerDto.getEmail());
         return user;
     }
 }
