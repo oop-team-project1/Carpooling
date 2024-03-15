f414b7f851b4623aa32f4335b4f51a4a8c42a846
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-15 17:05:54 +0200

    Added register logic in AuthenticationMvcController

--- Carpooling/src/main/java/com/company/carpooling/controllers/mvc/AuthenticationMvcController.java
+++ Carpooling/src/main/java/com/company/carpooling/controllers/mvc/AuthenticationMvcController.java
@@ -1,9 +1,12 @@
 package com.company.carpooling.controllers.mvc;
 
 import com.company.carpooling.exceptions.AuthorizationException;
+import com.company.carpooling.exceptions.EntityDuplicateException;
 import com.company.carpooling.helpers.AuthenticationHelper;
+import com.company.carpooling.helpers.UserMapper;
 import com.company.carpooling.models.User;
 import com.company.carpooling.models.dtos.LoginDto;
+import com.company.carpooling.models.dtos.RegisterDto;
 import com.company.carpooling.services.UserService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -20,13 +23,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/auth")
 public class AuthenticationMvcController {
+    public static final String PASSWORD_CONFIRMATION_ERROR = "Password confirmation should match password.";
     private final UserService userService;
     private final AuthenticationHelper authenticationHelper;
+    private final UserMapper userMapper;
 
     @Autowired
-    public AuthenticationMvcController(UserService userService, AuthenticationHelper authenticationHelper) {
+    public AuthenticationMvcController(UserService userService, AuthenticationHelper authenticationHelper, UserMapper userMapper) {
         this.userService = userService;
         this.authenticationHelper = authenticationHelper;
+        this.userMapper = userMapper;
     }
 
     @ModelAttribute("isAuthenticated")
@@ -68,5 +74,32 @@ public class AuthenticationMvcController {
     public String handleLogout(HttpSession session) {
         session.removeAttribute("currentUser");
         return "redirect:/auth/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage (Model model) {
+        model.addAttribute("register", new RegisterDto());
+        return "RegisterView";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@Valid @ModelAttribute("register") RegisterDto register,
+                                 BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "RegisterView";
+        }
+        if(!register.getPassword().equals(register.getPasswordConfirm())) {
+            bindingResult.rejectValue("passwordConfirm", "password_error", PASSWORD_CONFIRMATION_ERROR);
+            return "RegisterView";
+        }
+
+        try {
+            User user = userMapper.fromDtoRegister(register);
+            userService.create(user);
+            return "redirect:/auth/login";
+        } catch (EntityDuplicateException e) {
+            bindingResult.rejectValue("username", "username_error", e.getMessage());
+            return "RegisterView";
+        }
     }
 }
