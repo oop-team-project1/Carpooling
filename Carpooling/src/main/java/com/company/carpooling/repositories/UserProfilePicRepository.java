041d99f0d7f4c28d308797ee69122eba4de859e7
Kaloyan <kaloyan.enchev@abv.bg>
2024-03-20 14:19:33 +0200

    Deleted UserProfilePicRepository

--- Carpooling/src/main/java/com/company/carpooling/repositories/UserProfilePicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.carpooling.repositories;
-
-import com.company.carpooling.models.UserProfilePic;
-
-public interface UserProfilePicRepository {
-    UserProfilePic getById(int id);
-}
