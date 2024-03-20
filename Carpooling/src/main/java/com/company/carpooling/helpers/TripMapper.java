a471622b0265935d9ec6928fe7ee36d97a7eba33
ivonasto <ivona.stoyanova.s@gmail.com>
2024-03-20 20:44:21 +0200

    map from tripDto with Coordinates (front end map)

--- Carpooling/src/main/java/com/company/carpooling/helpers/TripMapper.java
+++ Carpooling/src/main/java/com/company/carpooling/helpers/TripMapper.java
@@ -2,6 +2,7 @@ package com.company.carpooling.helpers;
 
 import com.company.carpooling.models.*;
 import com.company.carpooling.models.dtos.TripDto;
+import com.company.carpooling.models.dtos.TripDtoCoordinates;
 import com.company.carpooling.models.json.Point;
 import com.company.carpooling.repositories.TripRepository;
 import com.company.carpooling.services.BingMapsService;
@@ -14,6 +15,7 @@ public class TripMapper {
     private final BingMapsService bingMapService;
     private final AddressMapper addressMapper;
     private final TripRepository tripRepository;
+    private final PointMapper pointMapper;
 
     public Trip fromTripDto(TripDto dto) {
         Point startingPoint = bingMapService.getCoordinates(dto.getStartPoint());
@@ -30,6 +32,23 @@ public class TripMapper {
         trip.setSeatsAvailable(dto.getFreeSpots());
         return trip;
     }
+
+    public Trip fromTripDtoCoordinates(TripDtoCoordinates dto) {
+        Point startingPoint = pointMapper.fromDto(dto.getStartPoint());
+        Point endingPoint = pointMapper.fromDto(dto.getEndPoint());
+
+        Street startPoint = addressMapper.getAddress(startingPoint);
+        Street endPoint = addressMapper.getAddress(endingPoint);
+        Trip trip = new Trip();
+        bingMapService.setDistanceAndDuration(startingPoint,endingPoint, trip);
+        //BingServiceLogic for distance
+        trip.setDepartureTime(dto.getDepartureTime());
+        trip.setStartPoint(startPoint);
+        trip.setEndPoint(endPoint);
+        trip.setSeatsAvailable(dto.getFreeSpots());
+        return trip;
+    }
+
 
     public Trip fromTripDto(int id, TripDto tripDto) {
         Trip trip = fromTripDto(tripDto);
